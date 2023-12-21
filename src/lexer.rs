be64493af646eb9228f6baac083fd6be0effc2eb
moosipea <mattiasmcvolt@gmail.com>
2023-12-21 20:41:10 +0200

    no more executable

---
+++ src/lexer.rs
@@ -0,0 +1,6 @@
+use std::error::Error;
+use super::*;
+
+pub fn lex(src: &str) -> Result<&[Token], Box<dyn Error>> {
+    todo!()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,48 @@ pub enum Symbol<'a> {
     Number(i32),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Token<'a> {
     Open,
     Close,
     Symbol(Symbol<'a>),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::*;
+    use crate::lexer::*;
+
+    #[test]
+    fn lexer_basic() {
+        let sample = "(1, 2, 3)";
+        let expected = &[
+            Token::Open,
+            Token::Symbol(Symbol::Number(1)),
+            Token::Symbol(Symbol::Number(2)),
+            Token::Symbol(Symbol::Number(3)),
+            Token::Close,
+        ];
+        assert_eq!(lex(sample).expect("Lexing failed!"), expected)
+    }
+
+    #[test]
+    fn ast_basic() {
+        let sample = &[
+            Token::Open,
+            Token::Symbol(Symbol::Number(1)), 
+            Token::Symbol(Symbol::Number(2)), 
+            Token::Symbol(Symbol::Number(3)), 
+            Token::Close,
+        ];
+        let expected = Tree::Branch(vec![
+            Tree::Branch(vec![
+                Tree::Leaf(&Symbol::Number(1)),
+                Tree::Leaf(&Symbol::Number(2)),
+                Tree::Leaf(&Symbol::Number(3)),
+            ]),
+        ]);
+        assert_eq!(Tree::try_construct(sample).unwrap(), expected);
+    }
 }
