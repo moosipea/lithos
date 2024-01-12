7fbd3fa8c322725bc7784c3debab452ba7510f7f
moosipea <mattiasmcvolt@gmail.com>
2024-01-12 19:41:16 +0200

    add binop

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,7 +1,51 @@
 use std::collections::HashMap;
 
-use crate::ast::Ast;
+struct StringBuilder(String);
+impl StringBuilder {
+    fn empty() -> Self {
+        Self(String::new())
+    }
+    fn line(mut self, line: &str) -> Self {
+        self.0.push_str(line);
+        self.0.push('\n');
+        self
+    }
 
-fn eval_macro<'a>(name: &str, input: Ast) -> Ast<'a> {
-    todo!()
+    fn finish(self) -> String {
+        self.0
+    }
+}
+
+enum Op {
+    BinaryAdd,
+    BinarySub,
+    Load(u64),
+}
+
+impl Op {
+    fn asm(&self) -> String {
+        match self {
+            Op::BinaryAdd => StringBuilder::empty()
+                .line("pop rdx")
+                .line("pop rax")
+                .line("add rax, rdx")
+                .finish(),
+            Op::BinarySub => StringBuilder::empty()
+                .line("pop rdx")
+                .line("pop rax")
+                .line("sub rax, rdx")
+                .finish(),
+            Op::Load(value) => format!("push {value}\n"),
+        }
+    }
+}
+
+pub fn codegen() -> String {
+    let sample = &[Op::Load(34), Op::Load(35), Op::BinaryAdd];
+
+    sample
+        .into_iter()
+        .map(Op::asm)
+        .reduce(|result, chunk| format!("{result}{chunk}"))
+        .unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use rust_lisp_parser::{
     ast::{make_tree, Tree},
-    lexer::lex,
+    lexer::lex, codegen::codegen,
 };
 use std::process::ExitCode;
 
@@ -9,7 +9,10 @@ fn main() -> Result<ExitCode> {
     let src = "(add 34 35)";
     let tokens = lex(src)?;
     let tree = Tree::try_construct(&tokens)?;
-    let ast = make_tree(&tree)?;
+    let _ast = make_tree(&tree)?;
+
+    let asm = codegen();
+    println!("{asm}");
 
     Ok(ExitCode::SUCCESS)
 }
