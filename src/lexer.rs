656505ef15c303e66288d651a95f2f269211d1a7
Mattias Volt <mattiasmcvolt@gmail.com>
2024-01-27 18:52:04 +0200

    basic lexer v1000000

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,16 +1,3 @@
-#[derive(Debug)]
-pub struct Positioning {
-    line: usize,
-    column: usize,
-    length: usize,
-}
-
-impl Positioning {
-    fn new(line: usize, column: usize, length: usize) -> Self {
-	Self { line, column, length }
-    }
-}
-
 #[derive(Debug, PartialEq)]
 pub enum TokenKind<'a> {
     Open,
@@ -18,162 +5,76 @@ pub enum TokenKind<'a> {
     Symbol(&'a str),
 }
 
-#[derive(Debug)]
-pub struct Token<'a> {
-    kind: TokenKind<'a>,
-    pos: Positioning,
-}
-
-impl<'a> Token<'a> {
-    fn open_or_close(ch: char, line: usize) -> Option<Self> {
-	let kind = match ch {
-	    '(' => Some(TokenKind::Open),
-	    ')' => Some(TokenKind::Close),
-	    _ => None
-	}?;
-	Some(Self {
-	    kind,
-	    pos: Positioning::new(line, 0, 1),
-	})
-    }
-
-    fn symbol(content: &'a str, line: usize) -> Option<Self> {
-	if content.is_empty() {
-	    return None;
-	}
-	Some(Self {
-	    kind: TokenKind::Symbol(content),
-	    pos: Positioning::new(line, 0, content.len()),
-	})
-    }
-}
-
-struct StrBuffer<'a> {
-    slc: &'a str,
-    start: usize,
-    length: usize,
-}
-
-impl<'a> StrBuffer<'a> {
-    fn new(slc: &'a str) -> Self {
-	Self {
-	    slc,
-	    start: 0,
-	    length: 0,
-	}
-    }
-
-    fn reset(&mut self, start: usize) -> Option<()> {
-	if start >= self.slc.len() {
-	    return None;
-	}
-	self.start = start;
-	self.length = 0;
-	Some(())
-    }
-
-    fn extend_by_one(&mut self) -> Option<()> {
-	if self.start + self.length + 1 >= self.slc.len() {
-	    return None;
-	}
-	self.length += 1;
-	Some(())
-    }
-    
-    fn content(&self) -> Option<&'a str> {
-	self.slc.get(self.start..self.start + self.length)
-    }
-
-    fn is_empty(&self) -> bool {
-	self.length == 0
-    }
-}
-
-pub struct Lexer<'a> {
-    chars: Vec<char>,        
-    buffer: StrBuffer<'a>,
-    cursor: usize,
-    line: usize,
-    column: usize,
-}
-
-impl<'a> Lexer<'a> {
-    pub fn new(src: &'a str) -> Self {
-        Self {
-            chars: src.chars().collect(),
-	    buffer: StrBuffer::new(src),
-	    cursor: 0,
-	    line: 0,
-	    column: 0,
-        }
-    }
-
-    fn advance_cursor(&mut self) {
-	self.cursor += 1;
-	self.column += 1;
-    }
-}
-
-// N.B! Positions of token may be incorrect because of this
-fn remove_comments(string: &str) -> String {
-    let mut comment = false;
-    string.chars().filter(|ch| {
-	match ch {
-	    ';' => comment = true,
-	    '\n' => comment = false,
-	    _ => {}
-	}
-	!comment
-    }).collect()
-}
+//#[derive(Debug)]
+//pub struct Token<'a> {
+//    kind: TokenKind<'a>,
+//    pos: Positioning,
+//}
 
 pub fn preprocess(src: &str) -> String {
-    let normal_newlines = src.replace("\r\n", "\n");
-    remove_comments(&normal_newlines)
+    src.replace("\r\n", "\n")
 }
 
 // TODO: See: https://crates.io/crates/unicode-segmentation
 
-impl<'a> Iterator for Lexer<'a> {
-    type Item = Token<'a>;
-    fn next(&mut self) -> Option<Self::Item> {
-	loop {
-	    let ch = *self.chars.get(self.cursor)?;
+pub struct Tokeniser<'a> {
+    src: &'a str
+}
 
-	    if ch == '\n' {
-		self.line += 1;
-		self.column = 0;
-	    }
-	    
-	    match ch {
-		'(' | ')' => if self.buffer.is_empty() {
-		    let tok = Token::open_or_close(ch, self.line);
-		    self.advance_cursor();
-		    return tok;
-		} else {
-		    let tok = Token::symbol(self.buffer.content()?, self.line);
-		    self.buffer.reset(self.cursor)?;
-		    return tok;
-		},
-		w if w.is_whitespace() => {
-		    if !self.buffer.is_empty() {
-			let tok = Token::symbol(self.buffer.content()?, self.line);
-			self.advance_cursor();
-			self.buffer.reset(self.cursor)?;
-			return tok;
-		    } else {
-			self.advance_cursor();
-		    }
-		},
-		_ => {
-		    if self.buffer.is_empty() {
-			self.buffer.reset(self.cursor)?;
-		    }
-		    self.buffer.extend_by_one()?;
-		    self.advance_cursor();
-		}
+impl<'a> Tokeniser<'a> {
+    pub fn new(src: &'a str) -> Self {
+	Self { src }
+    }
+}
+
+impl<'a> Tokeniser<'a> {
+    fn take(&mut self, n: usize) -> Option<&'a str> {
+	let head = self.src.get(..n);
+	self.src = self.src.get(n..)?;
+	head
+    }
+
+    fn take_until<F: Fn(char) -> bool>(&mut self, predicate: F) -> Option<&'a str> {
+	for (i, ch) in self.src.chars().enumerate() {
+	    if predicate(ch) {
+		return self.take(i);
 	    }
 	}
+	None
+    }
+    
+    fn take_one(&mut self) -> Option<&'a str> {
+	self.take(1)
+    }
+
+    fn take_symbol(&mut self) -> Option<&'a str> {
+	self.take_until(|ch| ch == '(' || ch == ')' || ch.is_whitespace())
+    }
+
+    fn skip_comment(&mut self) -> Option<()> {
+	self.take_until(|ch| ch == '\n')?;
+	Some(())
+    }
+}
+
+impl<'a> Iterator for Tokeniser<'a> {
+    type Item = &'a str;
+    fn next(&mut self) -> Option<Self::Item> {
+	for ch in self.src.chars() {
+	    match ch {
+		';' => {
+		    self.skip_comment()?;
+		    return self.next();
+		},
+		'(' | ')' => return self.take_one(),		
+		ch if !ch.is_whitespace() => return self.take_symbol(),
+		_ => {
+		    self.take_one()?;
+		    return self.next();
+		}
+	    }
+	}	
+	None
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use rust_lisp_parser::lexer::*;
-use std::ops::RangeInclusive;
 
 fn read_one_of(paths: &[&str]) -> Option<String> {
     use std::fs;
@@ -13,25 +12,20 @@ fn read_one_of(paths: &[&str]) -> Option<String> {
     None
 }
 
-fn report_error(tokens: &[Token], range: RangeInclusive<usize>, code: &str) {
-    let sub = tokens.get(range).expect("Expected valid range");
-    let line = sub[0].pos.line;    
-    let prefix = format!("Error on line {line}: ");
-    let text = code.lines().nth(line);
-    println!("{prefix}{text}");
-}
-
 fn main() -> Result<()> {
     let src = read_one_of(&[
 	"test.pj",
 	"../test.pj"
     ]).expect("Expected to read file");
     
-    let code = preprocess(&src);   
-    let lexer = Lexer::new(&code);
-    let tokens: Vec<_> = lexer.collect();
+    let code = preprocess(&src);
+    println!("Source:\n{}\n", &code);
+    //let lexer = Lexer::new(&code);
+    //let tokens: Vec<_> = lexer.collect();
 
-    report_error(&tokens, 1..=1, &code);
+    for str_token in Tokeniser::new(&code) {
+	println!("{str_token:?}");
+    }
     
     Ok(())
 }
